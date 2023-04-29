ecdf32bb7ab86e004d294a2ce4d0f374b10fe181
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-29 13:22:05 +0600

    Larger Number show in javaScript input value
    Larger Number show in javaScript input value use if else condation

---
+++ larger_number_show.js
@@ -0,0 +1,7 @@
+let num1 = prompt("Enter 1st Number: ");
+let num2 = prompt("Enter 2nd Number: ");
+if (num1 > num2) {
+  document.write("Larger Number is: " + num1);
+} else {
+  document.write("larger Number is: " + num2);
+  }
